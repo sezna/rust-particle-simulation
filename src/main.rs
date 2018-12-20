e1c375ca1cc1f0a7ea98e383861c6c6daba5a007
Hansen, Alexander (PLV7380) <alexander.hansen@usaa.com>
2018-12-20 07:14:29 -0600

    restart of project

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,38 @@
 extern crate image;
 extern crate rand;
-use std::thread;
 use image::{ImageBuffer, Rgb};
 use rand::{thread_rng, Rng};
-#[derive(Copy, Clone)]
+use std::thread;
+
+#[derive(Clone)]
 struct Element {
     stickiness: f64,
+    name: String,
+    color: [u8; 3],
 }
+
 impl Element {
     fn hydrogen() -> Element {
         return Element {
-            // name: "hydrogen".to_string(),
+            name: String::from("hydrogen"),
             stickiness: 0.6,
+            color: [255, 255, 255],
+        };
+    }
+
+    fn oxygen() -> Element {
+        return Element {
+            name: String::from("oxygen"),
+            stickiness: 0.2,
+            color: [0, 255, 100],
         };
     }
 }
 
-#[derive(Copy, Clone)]
+// TODO factor out into point_3d
+// make combine names function
+// utils dir, color struct
+#[derive(Clone)]
 struct Particle {
     x: f64,
     y: f64,
@@ -25,13 +41,23 @@ struct Particle {
     vy: f64,
     vz: f64,
     radius: f64,
-    element_type: Element,
+    element: Element,
 }
+
 fn distance((x1, y1): (u32, u32), (x2, y2): (u32, u32)) -> u32 {
-    return (((x1 as f64 - x2 as f64).powi(2) + (y1 as f64 - y2 as f64).powi(2))).sqrt() as u32;
+    return ((x1 as f64 - x2 as f64).powi(2) + (y1 as f64 - y2 as f64).powi(2)).sqrt() as u32;
 }
+
+fn average_color(color_one: [u8; 3], color_two: [u8; 3]) -> [u8; 3] {
+    [
+        color_one[0] + color_two[0] / 2,
+        color_one[1] + color_two[1] / 2,
+        color_one[2] + color_two[2] / 2,
+    ]
+}
+
 impl Particle {
-    fn low_energy_particle(x: f64, y: f64, z: f64) -> Particle {
+    fn low_energy_hydrogen(x: f64, y: f64, z: f64) -> Particle {
         let mut rng = thread_rng();
         return Particle {
             x: x,
@@ -41,10 +67,38 @@ impl Particle {
             vy: rng.gen_range(-0.5, 0.5),
             vz: rng.gen_range(-0.5, 0.5),
             radius: rng.gen_range(0.3, 1.4),
-            element_type: Element::hydrogen(),
+            element: Element::hydrogen(),
         };
-
     }
+
+    fn low_energy_oxygen(x: f64, y: f64, z: f64) -> Particle {
+        let mut rng = thread_rng();
+        return Particle {
+            x: x,
+            y: y,
+            z: z,
+            vx: rng.gen_range(-0.5, 0.5),
+            vy: rng.gen_range(-0.5, 0.5),
+            vz: rng.gen_range(-0.5, 0.5),
+            radius: rng.gen_range(0.3, 1.4),
+            element: Element::oxygen(),
+        };
+    }
+
+    fn high_energy_oxygen(x: f64, y: f64, z: f64) -> Particle {
+        let mut rng = thread_rng();
+        return Particle {
+            x: x,
+            y: y,
+            z: z,
+            vx: rng.gen_range(-10.5, 10.5),
+            vy: rng.gen_range(-10.5, 10.5),
+            vz: rng.gen_range(-10.5, 10.5),
+            radius: rng.gen_range(0.3, 3.4),
+            element: Element::oxygen(),
+        };
+    }
+
     fn time_step_return(&self) -> Particle {
         return Particle {
             x: self.x + self.vx,
@@ -54,13 +108,14 @@ impl Particle {
             vy: self.vy,
             vz: self.vz,
             radius: self.radius,
-            element_type: self.element_type,
+            element: self.element.clone(),
         };
     }
     fn distance(&self, other: &Particle) -> f64 {
-        return ((other.x - self.x).powi(2) + (other.y - self.y).powi(2) +
-                (other.z - self.z).powi(2))
-                   .sqrt();
+        return ((other.x - self.x).powi(2)
+            + (other.y - self.y).powi(2)
+            + (other.z - self.z).powi(2))
+        .sqrt();
     }
     fn did_collide(&self, other: &Particle) -> bool {
         return self.distance(other).abs() < (self.radius + other.radius);
@@ -68,7 +123,7 @@ impl Particle {
     fn will_stick(&self, other: &Particle) -> bool {
         let mut rng = thread_rng();
         let rand: f64 = rng.gen_range(0.0, 1.0);
-        return rand > (self.element_type.stickiness + other.element_type.stickiness / 2.0);
+        return rand > (self.element.stickiness + other.element.stickiness / 2.0);
     }
     fn sticky_collision(&self, other: &Particle) -> Particle {
         let new_radius = (self.radius.powi(3) + other.radius.powi(3)).cbrt();
@@ -80,11 +135,14 @@ impl Particle {
             vy: self.vy + other.vy,
             vz: self.vz + other.vz,
             radius: new_radius,
-            element_type: Element {
-                stickiness: (self.element_type.stickiness + other.element_type.stickiness) / 2.0,
+            element: Element {
+                name: format!("{}-{}", self.element.name, other.element.name),
+                stickiness: (self.element.stickiness + other.element.stickiness) / 2.0,
+                color: average_color(self.element.color, other.element.color),
             },
         };
     }
+
     fn bounds_check(&self, height: f64, width: f64, length: f64) -> Particle {
         let mut return_particle: Particle = self.clone();
 
@@ -125,30 +183,37 @@ impl Particle {
         particle2.vz = self.vz;
         return (particle1, particle2);
     }
-
 }
 
 fn main() {
     let height = 1;
     let width = 1000;
     let length = 1000;
-    let distribution = 0.92; // units per Particle
+    let distribution = 15.0; // units per Particle
     let mut field: Vec<Particle> = Vec::new();
     for z in 0..height {
+        println!("height: {}", z);
         for x in 0..width {
             for y in 0..length {
                 let mut rng = thread_rng();
                 let maybe = rng.gen_range(0.0, 1.0);
                 if maybe > distribution {
-                    field.push(Particle::low_energy_particle(x as f64, y as f64, z as f64));
-                }
+                    // We are adding a particle, but now we see which kind.
 
+                    if maybe < 0.33 {
+                        field.push(Particle::low_energy_hydrogen(x as f64, y as f64, z as f64));
+                    } else if maybe < 0.66 {
+                        field.push(Particle::low_energy_oxygen(x as f64, y as f64, z as f64));
+                    } else {
+                        field.push(Particle::high_energy_oxygen(x as f64, y as f64, z as f64));
+                    }
+                }
             }
         }
     }
-let mut frame = 0;
-loop {
-    if field.len() == 0 {
+    let mut frame = 0;
+    loop {
+        if field.len() < 5 || frame == 60 {
             break;
         }
         println!("time step: {}", frame);
@@ -157,7 +222,7 @@ loop {
             field[i] = field[i].time_step_return();
         }
         // Check for collisions
-        let mut particle_coords:Vec<(i32, i32, f64)> = Vec::new();
+        let mut particle_coords: Vec<(i32, i32, f64)> = Vec::new();
         let mut i = 0 as usize;
         let mut j = 0 as usize;
         loop {
@@ -194,35 +259,39 @@ loop {
         // Output map
         println!("drawing frame {}", frame);
 
-        fn closurefunc(particles: Vec<(i32, i32, f64)>, frame_number: i32, width_local: i32, length_local: i32) {
-        let mut img = ImageBuffer::<Rgb<u8>>::new(width_local as u32, length_local as u32);
-        for (x, y, radius)  in particles { 
-            let x1 = x as u32;
-            let y1 = y as u32;
-            let mut lower_x = x1 as i32 - radius as i32;
-            if lower_x < 0 {
-                lower_x = 0;
-            }
-            let mut lower_y = y1 as i32 - radius as i32;
-            if lower_y < 0 {
-                lower_y = 0;
-            }
-            for x2 in lower_x as u32..(x1 + (radius as u32)) {
-                for y2 in lower_y as u32..(y1 + (radius as u32)) {
-                    if distance((x1, y1), (x2, y2)) < radius as u32 {
-
-                        if x2 < width_local as u32 && y2 < length_local as u32 {
-                            img.get_pixel_mut(x2, y2).data = [255, 255, 255];
+        fn closurefunc(
+            particles: Vec<(i32, i32, f64)>,
+            frame_number: i32,
+            width_local: i32,
+            length_local: i32,
+        ) {
+            let mut img = ImageBuffer::<Rgb<u8>, _>::new(width_local as u32, length_local as u32);
+            for (x, y, radius) in particles {
+                let x1 = x as u32;
+                let y1 = y as u32;
+                let mut lower_x = x1 as i32 - radius as i32;
+                if lower_x < 0 {
+                    lower_x = 0;
+                }
+                let mut lower_y = y1 as i32 - radius as i32;
+                if lower_y < 0 {
+                    lower_y = 0;
+                }
+                for x2 in lower_x as u32..(x1 + (radius as u32)) {
+                    for y2 in lower_y as u32..(y1 + (radius as u32)) {
+                        if distance((x1, y1), (x2, y2)) < radius as u32 {
+                            if x2 < width_local as u32 && y2 < length_local as u32 {
+                                img.get_pixel_mut(x2, y2).data = [255, 255, 255];
+                            }
                         }
                     }
                 }
             }
-        }
-        img.save(format!("frame{:09}_output.png", frame_number)).unwrap();
-        return;
+            img.save(format!("frame{:09}_output.png", frame_number))
+                .unwrap();
+            return;
         };
         thread::spawn(move || closurefunc(particle_coords, frame, width, length));
-    frame = frame + 1;
+        frame = frame + 1;
     }
 }
-
