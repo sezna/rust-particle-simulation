f62a4dd9efb5d53aaa60f10e2e62ad19700e5bfb
Hansen, Alexander (PLV7380) <alexander.hansen@usaa.com>
2018-12-20 09:58:17 -0600

    added different colors

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ impl Element {
         return Element {
             name: String::from("hydrogen"),
             stickiness: 0.6,
-            color: [255, 255, 255],
+            color: [255, 0, 0],
         };
     }
 
@@ -24,7 +24,7 @@ impl Element {
         return Element {
             name: String::from("oxygen"),
             stickiness: 0.2,
-            color: [0, 255, 100],
+            color: [0, 255, 0],
         };
     }
 }
@@ -32,6 +32,12 @@ impl Element {
 // TODO factor out into point_3d
 // make combine names function
 // utils dir, color struct
+// redo image making, it currently is really slow
+// when a big particle eats a small one, make the position the big one 
+// gravity
+// elastic collision proportional to size, not just straight velocity transfer
+// 
+// maybe make combined particles not a circle? like stick to the outside?
 #[derive(Clone)]
 struct Particle {
     x: f64,
@@ -49,11 +55,13 @@ fn distance((x1, y1): (u32, u32), (x2, y2): (u32, u32)) -> u32 {
 }
 
 fn average_color(color_one: [u8; 3], color_two: [u8; 3]) -> [u8; 3] {
-    [
-        color_one[0] + color_two[0] / 2,
-        color_one[1] + color_two[1] / 2,
-        color_one[2] + color_two[2] / 2,
-    ]
+    let (r1, g1, b1) = ( color_one[0] as u16, color_one[1] as u16, color_one[2] as u16 );
+    let (r2, g2, b2) = ( color_two[0] as u16, color_two[1] as u16, color_two[2] as u16 );
+
+   return [ if r1 + r2 > 512u16 { 255u8 } else { ((r1 + r2) / 2) as u8 }, 
+            if g1 + g2 > 512u16 { 255u8 } else { ((g1 + g2) / 2) as u8 },
+            if b1 + b2 > 512u16 { 255u8 } else { ((b1 + b2) / 2) as u8 },
+   ];
 }
 
 impl Particle {
@@ -66,7 +74,7 @@ impl Particle {
             vx: rng.gen_range(-0.5, 0.5),
             vy: rng.gen_range(-0.5, 0.5),
             vz: rng.gen_range(-0.5, 0.5),
-            radius: rng.gen_range(0.3, 1.4),
+            radius: rng.gen_range(3.3, 5.4),
             element: Element::hydrogen(),
         };
     }
@@ -80,7 +88,7 @@ impl Particle {
             vx: rng.gen_range(-0.5, 0.5),
             vy: rng.gen_range(-0.5, 0.5),
             vz: rng.gen_range(-0.5, 0.5),
-            radius: rng.gen_range(0.3, 1.4),
+            radius: rng.gen_range(3.3, 5.4),
             element: Element::oxygen(),
         };
     }
@@ -189,7 +197,7 @@ fn main() {
     let height = 1;
     let width = 1000;
     let length = 1000;
-    let distribution = 15.0; // units per Particle
+    let distribution = 0.98; // units per Particle
     let mut field: Vec<Particle> = Vec::new();
     for z in 0..height {
         println!("height: {}", z);
@@ -198,14 +206,12 @@ fn main() {
                 let mut rng = thread_rng();
                 let maybe = rng.gen_range(0.0, 1.0);
                 if maybe > distribution {
+                    let  which_particle = rng.gen_range(0.0, 1.0);
                     // We are adding a particle, but now we see which kind.
-
-                    if maybe < 0.33 {
+                    if which_particle < 0.4 {
                         field.push(Particle::low_energy_hydrogen(x as f64, y as f64, z as f64));
-                    } else if maybe < 0.66 {
-                        field.push(Particle::low_energy_oxygen(x as f64, y as f64, z as f64));
                     } else {
-                        field.push(Particle::high_energy_oxygen(x as f64, y as f64, z as f64));
+                        field.push(Particle::low_energy_oxygen(x as f64, y as f64, z as f64));
                     }
                 }
             }
@@ -213,7 +219,8 @@ fn main() {
     }
     let mut frame = 0;
     loop {
-        if field.len() < 5 || frame == 60 {
+        if field.len() < 5 || frame == 260 {
+            println!("breaking: {} {}", field.len(), frame);
             break;
         }
         println!("time step: {}", frame);
@@ -222,11 +229,11 @@ fn main() {
             field[i] = field[i].time_step_return();
         }
         // Check for collisions
-        let mut particle_coords: Vec<(i32, i32, f64)> = Vec::new();
+        let mut particle_coords: Vec<(i32, i32, f64, [u8; 3])> = Vec::new();
         let mut i = 0 as usize;
         let mut j = 0 as usize;
         loop {
-            particle_coords.push((field[i].x as i32, field[i].y as i32, field[i].radius));
+            particle_coords.push((field[i].x as i32, field[i].y as i32, field[i].radius, field[i].element.color));
             loop {
                 field[i] = field[i].bounds_check(height as f64, width as f64, length as f64);
                 field[j] = field[j].bounds_check(height as f64, width as f64, length as f64);
@@ -253,20 +260,21 @@ fn main() {
             j = 0 as usize;
             i = i + 1;
             if i >= field.len() || j >= field.len() {
+                println!("done calculating frame");
                 break;
             }
         }
         // Output map
-        println!("drawing frame {}", frame);
+        print!("drawing frame {}... ", frame);
 
         fn closurefunc(
-            particles: Vec<(i32, i32, f64)>,
+            particles: Vec<(i32, i32, f64, [u8; 3])>,
             frame_number: i32,
             width_local: i32,
             length_local: i32,
         ) {
             let mut img = ImageBuffer::<Rgb<u8>, _>::new(width_local as u32, length_local as u32);
-            for (x, y, radius) in particles {
+            for (x, y, radius, color) in particles {
                 let x1 = x as u32;
                 let y1 = y as u32;
                 let mut lower_x = x1 as i32 - radius as i32;
@@ -281,17 +289,19 @@ fn main() {
                     for y2 in lower_y as u32..(y1 + (radius as u32)) {
                         if distance((x1, y1), (x2, y2)) < radius as u32 {
                             if x2 < width_local as u32 && y2 < length_local as u32 {
-                                img.get_pixel_mut(x2, y2).data = [255, 255, 255];
+                                img.get_pixel_mut(x2, y2).data = color ;
                             }
                         }
                     }
                 }
             }
+            println!("saving  frame...");
             img.save(format!("frame{:09}_output.png", frame_number))
                 .unwrap();
             return;
         };
-        thread::spawn(move || closurefunc(particle_coords, frame, width, length));
+//        thread::spawn(move || closurefunc(particle_coords, frame, width, length));
+        closurefunc(particle_coords, frame, width, length);
         frame = frame + 1;
     }
 }
